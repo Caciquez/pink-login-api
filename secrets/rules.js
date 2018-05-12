70d4e292f848090a26ae486a15f317d8b900bced
Caciquez <victorcaciquinho@gmail.com>
2018-05-12 16:06:08 -0300

    .gitignore created.

--- secrets/rules.js
+++ secrets/rules.js
@@ -1,28 +1,27 @@
-'use strict';
+
 
 const pgKnexAWS = {
-    client: 'pg',
-    connection: {
-        host: '54.173.239.35',
-        user: 'cacique',
-        password: 'Aa123456',
-        database: 'postgres'
-    }
-}
+  client: 'pg',
+  connection: {
+    host: '54.173.239.35',
+    user: 'cacique',
+    password: 'Aa123456',
+    database: 'postgres',
+  },
+};
 
 const pgKnexLocal = {
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'skyline123',
-        database: 'webTest'
-    }
-}
-
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'skyline123',
+    database: 'webTest',
+  },
+};
 
 
 module.exports = {
-    pgKnexAWS,
-    pgKnexLocal
-}
+  pgKnexAWS,
+  pgKnexLocal,
+};
